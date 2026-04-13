b3a226eed99711bee130aeaeeec65f8f9e09bb20
Selimmyrat <gandymovspc@gmail.com>
2026-04-13 22:42:33 +0500

    added authlayout, route, page

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import { RootLayout } from "../layouts/RootLayout";
 import { HomePage } from "@/pages/home";
 import { NotFoundPage } from "@/pages/not-found";
+import { AuthLayout } from "../layouts/AuthLayout";
+import { LoginPage } from "@/pages/login";
 
 export function AppRouter() {
   return (
@@ -9,6 +11,9 @@ export function AppRouter() {
       <Route element={<RootLayout />}>
         <Route index element={<HomePage />} />
         <Route path="*" element={<NotFoundPage />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   );
